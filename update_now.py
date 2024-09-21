a55424c8abead379bda1c387df967fa3008c6188
nguyenlukieuan <nguyenlukieuan@gmail.com>
2024-09-21 14:19:24 +0000

    save status

---
+++ update_now.py
@@ -0,0 +1,11 @@
+import subprocess
+script_path = """
+git add . && git commit -m "save status" && git push 
+
+"""
+
+try:
+    subprocess.run(script_path, shell=True, check=True)
+    print("Backup database completed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error executing the shell command: {e}")
